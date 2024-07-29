75342f467c40654c608f7cf7e961b819e1df7e4d
indigoxela <user@olga.indigofloat.at>
2024-07-29 16:52:48 +0200

    Initial commit with working code

---
+++ js/plugins/js_accordion/plugin.js
@@ -0,0 +1,190 @@
+/**
+ * @file
+ * TinyMCE Javascript Accordion plugin.
+ */
+(function () {
+
+  "use strict";
+
+  const dlInsertBefore = function (editor) {
+    let node = editor.selection.getNode();
+    let targetDt;
+    if (node.nodeName === 'DT') {
+      targetDt = node;
+    }
+    else if (node.nodeName === 'DD' && node.previousSibling.nodeName === 'DT') {
+      targetDt = node.previousSibling;
+    }
+    else {
+      while ((node = node.parentNode)) {
+        if (node.nodeName === 'DL') {
+          break;
+        }
+        if (node.nodeName === 'DT') {
+          targetDt = node;
+          break;
+        }
+        if (node.nodeName === 'DD' && node.previousSibling.nodeName === 'DT') {
+          targetDt = node.previousSibling;
+          break;
+        }
+      }
+    }
+    if (targetDt) {
+      editor.undoManager.transact( function () {
+        let dt = editor.dom.create('dt', {}, 'New accordion title');
+        let dd = editor.dom.create('dd');
+        dd.innerHTML = '<p>New accordion content.</p>';
+        targetDt.parentNode.insertBefore(dt, targetDt);
+        targetDt.parentNode.insertBefore(dd, targetDt);
+      });
+    }
+  };
+
+  const dlInsertAfter = function (editor) {
+    let node = editor.selection.getNode();
+    let targetDd;
+    if (node.nodeName === 'DD') {
+      targetDd = node;
+    }
+    else if (node.nodeName === 'DT' && node.nextSibling.nodeName === 'DD') {
+      targetDd = node.nextSibling;
+    }
+    else {
+      while ((node = node.parentNode)) {
+        if (node.nodeName === 'DL') {
+          break;
+        }
+        if (node.nodeName === 'DD') {
+          targetDd = node;
+          break;
+        }
+        if (node.nodeName === 'DT' && node.nextSibling.nodeName === 'DD') {
+          targetDd = node.nextSibling;
+          break;
+        }
+      }
+    }
+
+    if (targetDd) {
+      editor.undoManager.transact( function () {
+        let dt = editor.dom.create('dt', {}, 'New accordion title');
+        let dd = editor.dom.create('dd');
+        dd.innerHTML = '<p>New accordion content.</p>';
+        targetDd.parentNode.insertBefore(dd, targetDd.nextSibling);
+        targetDd.parentNode.insertBefore(dt, targetDd.nextSibling);
+      });
+    }
+  };
+
+  const dlDelItem = function (editor) {
+    let node = editor.selection.getNode();
+    let targetNode;
+    if (node.nodeName === 'DT' || node.nodeName === 'DD') {
+      targetNode = node;
+    }
+    else {
+      while ((node = node.parentNode)) {
+        if (node.nodeName === 'DL') {
+          break;
+        }
+        if (node.nodeName === 'DT' || node.nodeName === 'DD') {
+          targetNode = node;
+          break;
+        }
+      }
+    }
+
+    if (targetNode) {
+      let delNodes = [];
+      delNodes.push(targetNode);
+      if (targetNode.nodeName === 'DT' && targetNode.nextSibling.nodeName === 'DD') {
+        delNodes.push(targetNode.nextSibling);
+      }
+      else if (targetNode.nodeName === 'DD' && targetNode.previousSibling.nodeName === 'DT') {
+        delNodes.push(targetNode.previousSibling);
+      }
+      editor.undoManager.transact( function () {
+        delNodes.forEach(function (element) {
+          editor.dom.remove(element);
+        });
+      });
+    }
+  };
+
+  tinymce.PluginManager.add('js_accordion', function (editor, url) {
+    editor.ui.registry.addButton('js_accordion', {
+      icon: 'js-accordion',
+      tooltip: editor.options.get('tinyJsAccToolbarButtonText'),
+      onAction: function () {
+        let node = editor.selection.getNode();
+        if (node.closest('dl.ckeditor-accordion')) {
+          editor.notificationManager.open({
+            text: editor.options.get('tinyJsAccWarnMessageText'),
+            timeout: 5000,
+            type: 'warning'
+          });
+          return;
+        }
+        let content = '<dl class="ckeditor-accordion">';
+        content += '<dt>Accordion title 1</dt><dd><p>Accordion content 1.</p></dd>';
+        content += '<dt>Accordion title 2</dt><dd><p>Accordion content 2.</p></dd>';
+        content += '</dl>';
+        editor.insertContent(content);
+      },
+      onSetup: function (api) {
+        editor.on('keydown', function (event) {
+          if (event.keyCode !== 13) {
+            return;
+          }
+          if (event.target.nodeName === 'DT') {
+            event.preventDefault();
+          }
+        });
+        editor.on('NewBlock', function (event) {
+          if (event.newBlock.nodeName === 'DT') {
+            let prevContent = event.newBlock.previousSibling.innerHTML;
+            let thisContent = event.newBlock.innerHTML;
+            event.newBlock.previousSibling.innerHTML = prevContent + thisContent;
+            editor.selection.setCursorLocation(event.newBlock.previousSibling, 1);
+            editor.dom.remove(event.newBlock);
+          }
+        });
+      }
+    });
+
+    editor.ui.registry.addButton('desclistInsertBefore', {
+      icon: 'dd-add-before',
+      tooltip: editor.options.get('tinyJsAccInsertBeforeButtonText'),
+      onAction: function () {
+        dlInsertBefore(editor);
+      }
+    });
+
+    editor.ui.registry.addButton('desclistInsertAfter', {
+      icon: 'dd-add-after',
+      tooltip: editor.options.get('tinyJsAccInsertAfterButtonText'),
+      onAction: function () {
+        dlInsertAfter(editor);
+      }
+    });
+
+    editor.ui.registry.addButton('desclistDelItem', {
+      icon: 'remove',
+      tooltip: editor.options.get('tinyJsAccDeleteButtonText'),
+      onAction: function () {
+        dlDelItem(editor);
+      }
+    });
+
+    editor.ui.registry.addContextToolbar('desclistbar', {
+      predicate: function (node) {
+        return (node.nodeName === 'DT' || node.nodeName === 'DD');
+      },
+      items: 'desclistInsertBefore desclistInsertAfter desclistDelItem',
+      scope: 'node',
+      position: 'node'
+    });
+  });
+
+})();
---
+++ js/tinymce-js-accordion.js
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * Backdrop behaviors for Javascript animated accordions based on dl/dt/dd.
+ *
+ * Mostly just a copy of CKEditor Accordion functionality.
+ */
+
+(function ($) {
+  'use strict';
+  Backdrop.behaviors.tinymceDesclist = {
+    attach: function (context, settings) {
+
+      var $ckeditorAccordion = $('.ckeditor-accordion');
+      if (!$ckeditorAccordion.length || typeof Backdrop.behaviors.ckeditorAccordion !== 'undefined') {
+        // Either nothing to do, or the CKEditor plugin's already active.
+        return;
+      }
+      if ($ckeditorAccordion.length > 0) {
+        // Create simple accordion mechanism for each tab.
+        $ckeditorAccordion.each(function () {
+          var $accordion = $(this);
+          if ($accordion.hasClass('styled')) {
+            return;
+          }
+
+          // The first one is the correct one.
+          if (typeof Backdrop.settings.tinymce_js_accordion === 'undefined' ||
+             (typeof Backdrop.settings.tinymce_js_accordion !== 'undefined' && !Backdrop.settings.tinymce_js_accordion.collapseAll)) {
+            $accordion.children('dt:first').addClass('active');
+            $accordion.children('dd:first').addClass('active');
+            $accordion.children('dd:first').css('display', 'block');
+          }
+
+          // Turn the accordion tabs to links so that the content is accessible & can be traversed using keyboard.
+          $accordion.children('dt').each(function () {
+            var $tab = $(this);
+            var tabText = $tab.text().trim();
+            var toggleClass = $tab.hasClass('active') ? ' active' : '';
+            $tab.html('<span class="ckeditor-accordion-toggle' + toggleClass + '"></span><a class="ckeditor-accordion-toggler" href="#">' + tabText + '</a>');
+          });
+
+          // Wrap the accordion in a div element so that quick edit function shows the source correctly.
+          $accordion.addClass('styled').removeClass('ckeditor-accordion').wrap('<div class="ckeditor-accordion-container"></div>');
+
+          // Fire an event so other modules know when the accordion has been created.
+          $accordion.trigger('ckeditorAccordionAttached');
+        });
+
+        // Add click event to body once because quick edits & ajax calls might reset the HTML.
+        $('body').once('ckeditorAccordionToggleEvent').on('click', '.ckeditor-accordion-toggler', function (e) {
+          var $t = $(this).parent();
+          var $parent = $t.parent();
+
+          // Clicking on open element, close it.
+          if ($t.hasClass('active')) {
+            $t.removeClass('active');
+            $t.next().slideUp();
+          }
+          else {
+            // Remove active classes.
+            $parent.children('dt.active').removeClass('active').children('a').removeClass('active');
+            $parent.children('dd.active').slideUp(function () {
+              $(this).removeClass('active');
+            });
+
+            // Show the selected tab.
+            $t.addClass('active');
+            $t.next().slideDown(300).addClass('active');
+          }
+
+          // Don't add hash to url.
+          e.preventDefault();
+        });
+      }
+    }
+  };
+})(jQuery);
